fdd7ea2a794dab266c743fa81b7b1e51f9b5a0c6
Vincent <31508339+vbnin@users.noreply.github.com>
2024-11-26 16:18:04 +0100

    Update create_android_virtual_device.sh

--- create_android_virtual_device.sh
+++ create_android_virtual_device.sh
@@ -32,7 +32,7 @@
 # Editable variables
 SDK_ROOT="/Library/Android/SDK"    # Android CLI tools install path
 ANDROID_PACKAGE="system-images;android-35;google_apis_playstore;arm64-v8a"    # Android system image used to create the virtual device
-DEVICE_NAME="My Android Device"    # Name of your new Android virtual device
+DEVICE_NAME="My_Android_Device"    # Name of your new Android virtual device. No spaces allowed. 
 
 # Fixed variables
 SDK_BIN="${SDK_ROOT}/cmdline-tools/latest/bin"
